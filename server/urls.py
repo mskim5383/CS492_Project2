bbf67da1f781855ac50a1671b10677f1f31a5b75
rlaalstn5383 <rlaalstn5383@gmail.com>
2016-01-02 15:07:07 +0900

    user register 추가

--- server/urls.py
+++ server/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'server.views.waiting_room'),
+    url(r'^$', 'server.views.register'),
+    url(r'^([1-9][0-9]*)/$', 'server.views.waiting_room'),
+    url(r'^([1-9][0-9]*)/([1-9][0-9]*)/$', 'server.views.game_room'),
 ]
--- server/views.py
+++ server/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from server.models import *
 import json
 
 # Create your views here.
 
 
 
+def register(request):
+    player = Player()
+    player.save()
+    return HttpResponse(json.dumps({'id': player.id}), content_type="application/json")
 
-def waiting_room(request):
-    return HttpResponse(json.dumps({}), content_type="application/json")
+def waiting_room(request, user_id):
+    game_status_list = GameStatus.objects.all()
+    waiting_room_list = []
+    for game_status in game_status_list:
+        waiting_room_list.append(game_status.get_game_status())
+    return HttpResponse(json.dumps({'waiting_rooms': waiting_room_list}), content_type="application/json")
