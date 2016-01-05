d64b75db0066b8b9d68dc7e51d3db6369c9089f6
Xhark <ekzm0204@gmail.com>
2016-01-05 21:23:40 +0900

    Implement Table Layout
    카드가 놓일 위치를 지정함

--- frontend/Assets/Scripts/Table/TablePosition.cs
+++ frontend/Assets/Scripts/Table/TablePosition.cs
@@ -8,23 +8,128 @@ namespace Assets.Scripts.Table
 {
     class TablePosition
     {
+        private const int tableMargin = 30;
+        private const int cardMargin = 20;
+        private static int getPos(int centerPosition, int i, int n)
+        {
+            int startPosition = centerPosition - (cardMargin * (n - 1)) / 2;
+            return startPosition + cardMargin * i;
+        }
+
+        class Player5_Center
+        {
+            int player;
+            public Player5_Center(int player)
+            {
+                this.player = player;
+            }
+
+            public int x
+            {
+                get {
+                    switch (player)
+                    {
+                        case 0:
+                            return Screen.width / 2;
+                        case 1:
+                            return Screen.width - tableMargin;
+                        case 2:
+                            return Screen.width * 2 / 3;
+                        case 3:
+                            return Screen.width / 3;
+                        case 4:
+                            return tableMargin;
+                    }
+                    return 0;
+                }
+            }
+
+            public int y
+            {
+                get
+                {
+                    switch (player)
+                    {
+                        case 0:
+                            return tableMargin;
+                        case 1:
+                        case 4:
+                            return Screen.height / 2;
+                        case 2:
+                        case 3:
+                            return Screen.height - tableMargin;
+                    }
+                    return 0;
+                }
+            }
+
+            public bool isVertical
+            {
+                get
+                {
+                    return player == 1 || player == 4;
+                }
+            }
+
+            public bool isRev
+            {
+                get
+                {
+                    return player == 1 || player == 2 || player == 3;
+                }
+            }
+        };
+
         // 자신부터 반시계방향으로 0, 1, 2, 3, 4
         public static Vector3 getCardPositionForHand(int player, int i, int n)
         {
             Camera camera = Camera.main;
-            if (player == 0)
+            Player5_Center center = new Player5_Center(player);
+            if (center.isRev) i = n - 1 - i;
+            int x = center.x, y = center.y;
+            if (center.isVertical)
             {
-                int sx = Screen.width / 2;
-                int d = 20;
-                sx -= (d*(n-1))/ 2;
-                int x = sx + d * i;
-                int y = 30;
-
-                Vector3 pos = camera.ScreenToWorldPoint(new Vector3(x, y, 10));
-                return pos;
+                y = getPos(y, i, n);
             }
+            else
+            {
+                x = getPos(x, i, n);
+            }
+            return camera.ScreenToWorldPoint(new Vector3(x, y, 10));
+        }
+        
+        public static Vector3 getCardPositionForPointCard(int player, int i, int n)
+        {
+            Camera camera = Camera.main;
+            Player5_Center center = new Player5_Center(player);
+            if (center.isRev) i = n - 1 - i;
+            int x = center.x, y = center.y;
+            if (center.isVertical)
+            {
+                y = getPos(y, i, n);
+                if (player == 1) x -= tableMargin;
+                else x += tableMargin;
+            }
+            else
+            {
+                x = getPos(x, i, n);
+                if (player == 0) y += tableMargin;
+                else y -= tableMargin;
+            }
+            return camera.ScreenToWorldPoint(new Vector3(x, y, 10));
+        }
 
-            return new Vector3(0, 0, 0);
+        public static Vector3 getThrowPosition(int player)
+        {
+            Camera camera = Camera.main;
+            Player5_Center center = new Player5_Center(player);
+            int cx = Screen.width / 2;
+            int cy = Screen.height / 2;
+
+            int x = (center.x + cx) / 2;
+            int y = (center.y + cy) / 2;
+
+            return camera.ScreenToWorldPoint(new Vector3(x, y, 10));
         }
     }
 }
